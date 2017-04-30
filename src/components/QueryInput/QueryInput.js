5ae883c2feecab2e50985d2ac361b28b627e2b65
Julie Jarzemsky <jjarzemsky@Julies-MacBook-Air.local>
2017-04-30 12:29:13 -0500

    Responsive CSS

--- src/components/QueryInput/QueryInput.js
+++ src/components/QueryInput/QueryInput.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './QueryInput.css';
 
 function QueryInput(props) {
   const inputText =
@@ -12,8 +13,9 @@ WHERE {
 
   return (
     <textarea
-      name="query-input"
-      rows='10' cols='60'
+      className='query-input'
+      name='query-input'
+      rows='8' cols='40'
       value={inputText}
       readOnly>
     </textarea>
